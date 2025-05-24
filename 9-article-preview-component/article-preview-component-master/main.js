fecbb02008237825f33a2d4b2672da3a44e203d9
Antoine Belvire <antoine.belvire@opensuse.org>
2025-05-24 17:15:19 +0200

    Article preview component, tablet

--- 9-article-preview-component/article-preview-component-master/main.js
+++ 9-article-preview-component/article-preview-component-master/main.js
@@ -1,8 +1,12 @@
 const shareButtons = document.querySelectorAll('.share-button');
+const shareButtonIcons = document.querySelectorAll('.share-button__icon');
 const shareModal = document.querySelector('.share-modal');
 
 shareButtons.forEach(button => {
-    button.addEventListener('click', () =>
-        shareModal.classList.toggle('share-modal--hidden')
+    button.addEventListener('click', () => {
+            shareModal.classList.toggle('share-modal--hidden')
+            shareButtons.forEach(btn => btn.classList.toggle('share-button--toggled'))
+            shareButtonIcons.forEach(icon => icon.classList.toggle('share-button__icon--toggled'))
+        }
     )
 })
