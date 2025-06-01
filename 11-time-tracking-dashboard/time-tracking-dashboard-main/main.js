1952935682b09626e836e4d7c9985b34be859597
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-01 19:15:48 +0200

    Time tracking dashboard, create sections dynamically

--- 11-time-tracking-dashboard/time-tracking-dashboard-main/main.js
+++ 11-time-tracking-dashboard/time-tracking-dashboard-main/main.js
@@ -1,4 +1,61 @@
-const addSelectorListener = (selector, sections, otherSelectors, otherSelectorsSections) => {
+fetch("./data.json")
+    .then(response => response.json())
+    .then(populateReport)
+    .then(addSelectorListeners)
+
+function populateReport(data) {
+    const report = document.querySelector(".report")
+    for (const {title, timeframes} of data) {
+        const sectionElement = document.createElement("section")
+        sectionElement.classList.add("report-section", `report-section--${title.toLowerCase().replace(" ", "-")}`)
+        sectionElement.innerHTML = formatReportSectionContent({title, timeframes})
+        report.appendChild(sectionElement)
+    }
+}
+
+/* Formats the report section content based on the provided title and timeframes */
+function formatReportSectionContent({title, timeframes}) {
+    return `
+        <header class="report-section__header">
+            <h2 class="text-preset-5--medium">${title}</h2>
+        </header>
+        <div class="report-section__durations report-section__durations--daily">
+            <time class="current-duration text-preset-3">${timeframes["daily"].current}hrs</time>
+            <div class="text-preset-6">
+                Previous - <time class="previous-duration">${timeframes["daily"].previous}hrs</time>
+            </div>
+        </div>
+        <div class="report-section__durations report-section__durations--weekly report-section__durations--selected">
+            <time class="current-duration text-preset-3">${timeframes["weekly"].current}hrs</time>
+            <div class="text-preset-6">
+                Previous - <time class="previous-duration">${timeframes["weekly"].previous}hrs</time>
+            </div>
+        </div>
+        <div class="report-section__durations report-section__durations--monthly">
+            <time class="current-duration text-preset-3">${timeframes["monthly"].current}hrs</time>
+            <div class="text-preset-6">
+                Previous - <time class="previous-duration">${timeframes["monthly"].previous}hrs</time>
+            </div>
+        </div>
+        `
+}
+
+/* Adds event listeners to the selectors to toggle the active state and selected durations */
+function addSelectorListeners() {
+    const dailySelector = document.querySelector(".selectors__item--daily")
+    const dailySections = document.querySelectorAll(".report-section__durations--daily")
+    const weeklySelector = document.querySelector(".selectors__item--weekly")
+    const weeklySections = document.querySelectorAll(".report-section__durations--weekly")
+    const monthlySelector = document.querySelector(".selectors__item--monthly")
+    const monthlySections = document.querySelectorAll(".report-section__durations--monthly")
+
+    addSelectorListener(dailySelector, dailySections, [weeklySelector, monthlySelector], [weeklySections, monthlySections])
+    addSelectorListener(weeklySelector, weeklySections, [dailySelector, monthlySelector], [dailySections, monthlySections])
+    addSelectorListener(monthlySelector, monthlySections, [dailySelector, weeklySelector], [dailySections, weeklySections])
+}
+
+/* Adds a click event listener to the selector to toggle the active state and selected durations */
+function addSelectorListener(selector, sections, otherSelectors, otherSelectorsSections) {
     selector.addEventListener("click", e => {
         e.preventDefault()
         otherSelectors.forEach(otherSelector => otherSelector.classList.remove("selectors__item--active"))
@@ -11,30 +68,3 @@ const addSelectorListener = (selector, sections, otherSelectors, otherSelectorsS
         sections.forEach(section => section.classList.add("report-section__durations--selected"))
     })
 }
-
-const dailySelector = document.querySelector(".selectors__item--daily")
-const dailySections = document.querySelectorAll(".report-section__durations--daily")
-const weeklySelector = document.querySelector(".selectors__item--weekly")
-const weeklySections = document.querySelectorAll(".report-section__durations--weekly")
-const monthlySelector = document.querySelector(".selectors__item--monthly")
-const monthlySections = document.querySelectorAll(".report-section__durations--monthly")
-
-addSelectorListener(dailySelector, dailySections, [weeklySelector, monthlySelector], [weeklySections, monthlySections])
-addSelectorListener(weeklySelector, weeklySections, [dailySelector, monthlySelector], [dailySections, monthlySections])
-addSelectorListener(monthlySelector, monthlySections, [dailySelector, weeklySelector], [dailySections, weeklySections])
-
-fetch("./data.json")
-    .then(response => response.json())
-    .then(data => {
-        for (const {title, timeframes} of data) {
-            const sectionName = title.toLowerCase().replace(" ", "-")
-            const section = document.querySelector(`.report-section--${sectionName}`)
-            for (const timeframeName in timeframes) {
-                const durationsClass = `.report-section__durations--${timeframeName}`
-                const durations = section.querySelector(durationsClass)
-                const {previous, current} = timeframes[timeframeName]
-                durations.querySelector(".current-duration").textContent = current + "hrs"
-                durations.querySelector(".previous-duration").textContent = previous + "hrs"
-            }
-        }
-    })
