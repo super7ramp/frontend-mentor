e44d9959041e9be763164cbb9c335b3c1cdb2904
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-06 18:10:03 +0200

    Product list with cart, add to cart button states

--- 13-product-list-with-cart/product-list-with-cart-react/src/components/AddToCartButton.jsx
+++ 13-product-list-with-cart/product-list-with-cart-react/src/components/AddToCartButton.jsx
@@ -1,10 +1,45 @@
 import style from "./AddToCartButton.module.css"
 import addToCartImg from "../assets/images/icon-add-to-cart.svg"
+import incrementImg from "../assets/images/icon-increment-quantity.svg"
+import decrementImg from "../assets/images/icon-decrement-quantity.svg"
+import {useState} from "react";
 
 function AddToCartButton({className}) {
-    return <button className={`${className} ${style.btn} text-preset-4--bold`}>
+    const [count, setCount] = useState(0)
+    const [active, setActive] = useState(false)
+    return <>{
+        active
+            ? <ActiveAddToCartButton className={className}
+                                     currentCount={count}
+                                     setCount={setCount}
+                                     setInactive={() => setActive(false)}/>
+            : <InactiveAddToCartButton className={className}
+                                       setActive={() => setActive(true)}/>
+    }</>
+}
+
+function InactiveAddToCartButton({className, setActive}) {
+    return <button className={`${className} ${style.btn} ${style.btnInactive} text-preset-4--bold`}
+                   onClick={setActive}>
         <img src={addToCartImg} alt="Add to cart icon"/>
         Add to cart
+    </button>
+}
+
+function ActiveAddToCartButton({className, currentCount, setCount, setInactive}) {
+    const increment = () => setCount((count) => count + 1)
+    const decrement = () => setCount((count) => {
+        if (count > 0) {
+            return count - 1
+        }
+        setInactive()
+        return 0
+    })
+
+    return <button className={`${className} ${style.btn} ${style.btnActive} text-preset-4--bold`}>
+        <img src={decrementImg} alt="Decrement" onClick={decrement}/>
+        {currentCount}
+        <img src={incrementImg} alt="Increment" onClick={increment}/>
     </button>
 }
 
