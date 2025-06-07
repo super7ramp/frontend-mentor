2e2ee4ebaed3f02ecac46dc4db3ee6b22d677cae
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-07 19:08:57 +0200

    Product list with cart, desktop and tablet

--- 13-product-list-with-cart/product-list-with-cart-react/src/components/Dessert.jsx
+++ 13-product-list-with-cart/product-list-with-cart-react/src/components/Dessert.jsx
@@ -18,7 +18,7 @@ function Dessert({category, name, price, image, initialQuantity, onQuantityUpdat
         <div className={style.detailsContainer}>
             <p className={`${style.category} text-preset-4`}>{category}</p>
             <h1 className={`${style.name} text-preset-3`}>{name}</h1>
-            <p className={`${style.price} text-preset-3`}>{price}</p>
+            <p className={`${style.price} text-preset-3`}>{price.toFixed(2)}</p>
         </div>
     </article>
 }
@@ -27,8 +27,8 @@ function DessertPicture({image, name, selected}) {
     const styleImg = selected ? `${style.img} ${style.imgSelected}` : style.img;
     return (
         <picture>
-            <source srcSet={`/src/${image.tablet}`} media="(650px <= width < 1024px)"/>
-            <source srcSet={`/src/${image.desktop}`} media="(1024px <= width)"/>
+            <source srcSet={`/src/${image.tablet}`} media="(40.625rem <= width < 64rem)"/>
+            <source srcSet={`/src/${image.desktop}`} media="(64rem <= width)"/>
             <img className={styleImg} src={`/src/${image.mobile}`} alt={name}/>
         </picture>
     )
