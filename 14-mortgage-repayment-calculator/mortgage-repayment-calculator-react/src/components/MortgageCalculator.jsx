092823ee60aa4127228926fa8b1f714fcd6d0b34
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-09 17:19:58 +0200

    Mortgage repayment calculator, improve validation

--- 14-mortgage-repayment-calculator/mortgage-repayment-calculator-react/src/components/MortgageCalculator.jsx
+++ 14-mortgage-repayment-calculator/mortgage-repayment-calculator-react/src/components/MortgageCalculator.jsx
@@ -7,8 +7,8 @@ function MortgageCalculator() {
 
     const [result, setResult] = useState(null)
 
-    const onSubmit = (event) => {
-        event.preventDefault()
+    const onSubmit = (data) => {
+        console.log("Form submitted with data:", data);
         setResult({"monthly": 1_797.74, "total": 539_322.94})
     }
 
@@ -21,23 +21,89 @@ function MortgageCalculator() {
 }
 
 function MortgageCalculatorForm({onSubmit}) {
+
+    const [resets, setResets] = useState(0)
+    const [formData, setFormData] = useState({
+        amount: '',
+        term: '',
+        rate: '',
+        type: ''
+    });
+
+    const setAmount = (value) => setFormData({...formData, amount: value})
+    const setTerm = (value) => setFormData({...formData, term: value})
+    const setRate = (value) => setFormData({...formData, rate: value})
+    const setType = (value) => setFormData({...formData, type: value})
+    const setTypeRepayment = () => setType("repayment")
+    const setTypeInterestOnly = () => setType("interest-only")
+
+    const submit = (event) => {
+        event.preventDefault()
+        onSubmit(formData)
+    }
+
+    const reset = () => {
+        setFormData({
+            amount: '',
+            term: '',
+            rate: '',
+            type: ''
+        });
+        // increment resets to trigger re-render of NumberFields (and clear their local error states
+        setResets(resets + 1);
+    }
+
     return (
-        <form className={style.form} onSubmit={onSubmit}>
+        <form className={style.form}
+              onSubmit={submit}
+              onReset={reset}>
+
             <h1 className="text-preset-2">Mortgage Calculator</h1>
             <input type="reset" value="Clear All" className={`text-preset-4 ${style.resetFormButton}`}/>
-            <NumberField label="Mortgage Amount" prefix="£" min="0"/>
-            <NumberField label="Mortgage Term" suffix="years" min="1"/>
-            <NumberField label="Interest Rate" suffix="%" min="0" max="100"/>
+
+            <NumberField key={`amount-${resets}"`}
+                         label="Mortgage Amount"
+                         value={formData.amount}
+                         setValue={setAmount}
+                         prefix="£"
+                         min="0"/>
+
+            <NumberField key={`term-${resets}"`}
+                         label="Mortgage Term"
+                         value={formData.term}
+                         setValue={setTerm}
+                         suffix="years"
+                         min="1"/>
+
+            <NumberField key={`rate-${resets}"`}
+                         label="Interest Rate"
+                         value={formData.rate}
+                         setValue={setRate}
+                         suffix="%"
+                         min="0"
+                         max="100"/>
+
             <RadioGroup label="Mortgage Type">
-                <RadioField label="Repayment"/>
-                <RadioField label="Interest Only"/>
+
+                <RadioField label="Repayment"
+                            groupName="type"
+                            checked={formData.type === "repayment"}
+                            setChecked={setTypeRepayment}/>
+
+                <RadioField label="Interest Only"
+                            groupName="type"
+                            checked={formData.type === "interest-only"}
+                            setChecked={setTypeInterestOnly}/>
+
             </RadioGroup>
+
             <SubmitButton/>
+
         </form>
     )
 }
 
-function NumberField({label, prefix, suffix, min, max}) {
+function NumberField({label, value, setValue, prefix, suffix, min, max}) {
     const [error, setError] = useState(null)
     const onInput = (event) => {
         if (event.target.validity.valid) {
@@ -45,6 +111,7 @@ function NumberField({label, prefix, suffix, min, max}) {
         } else {
             setError(event.target.validationMessage)
         }
+        setValue(event.target.value)
     }
     return (
         <div className={style.numberField}>
@@ -57,6 +124,7 @@ function NumberField({label, prefix, suffix, min, max}) {
                        max={max || Number.MAX_VALUE}
                        step="any"
                        id={label}
+                       value={value}
                        className="text-preset-3"
                        onInput={onInput}/>
                 {suffix && <p className={`text-preset-3 ${style.numberFieldUnit}`}>{suffix}</p>}
@@ -67,18 +135,31 @@ function NumberField({label, prefix, suffix, min, max}) {
 }
 
 function RadioGroup({label, children}) {
+    const [error, setError] = useState(null)
     return (
         <div className={style.radioGroup}>
             <label className="text-preset-4">{label}</label>
             {children}
+            {error && <p className={`text-preset-5 ${style.error}`}>{error}</p>}
         </div>
     )
 }
 
-function RadioField({label}) {
+function RadioField({label, groupName, checked, setChecked}) {
+    const change = (event) => {
+        if (!event.target.validity.valid) {
+            console.log("Invalid radio input:", event.target.validationMessage);
+        }
+        setChecked()
+    }
     return (
         <div className={style.radioField}>
-            <input type="radio" id={label}/>
+            <input type="radio"
+                   id={label}
+                   name={groupName}
+                   required={true}
+                   checked={checked}
+                   onChange={change}/>
             <label htmlFor={label} className="text-preset-3">{label}</label>
         </div>
     )
