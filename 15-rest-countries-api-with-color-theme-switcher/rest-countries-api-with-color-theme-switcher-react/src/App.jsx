3ee66f780257c55f6a1e699eda5f5bceb1d6e709
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-11 23:52:49 +0200

    Rest Countries API, move main page to a dedicated file

--- 15-rest-countries-api-with-color-theme-switcher/rest-countries-api-with-color-theme-switcher-react/src/App.jsx
+++ 15-rest-countries-api-with-color-theme-switcher/rest-countries-api-with-color-theme-switcher-react/src/App.jsx
@@ -1,43 +1,8 @@
-import style from './App.module.scss'
-import MenuBar from "./components/MenuBar.jsx";
-import SearchBar from "./components/SearchBar.jsx";
-import DropDownOptions from "./components/DropDownOptions.jsx";
-import {useEffect, useState} from "react";
-import Card from "./components/Card.jsx";
-import CountriesApi from "./api/CountriesApi.js";
+import MainPage from "./pages/MainPage.jsx";
 
 function App() {
-    const [countries, setCountries] = useState([])
-    const [region, setRegion] = useState("")
-    const [search, setSearch] = useState("")
-    const lowerCaseSearch = search.toLowerCase()
-
-    useEffect(() => {
-        const countriesApi = new CountriesApi(import.meta.env.VITE_COUNTRIES_API)
-        countriesApi
-            .getAll()
-            .then(setCountries)
-    }, []);
-
     return (
-        <>
-            <MenuBar/>
-            <header className={style.header}>
-                <SearchBar value={search} onValueChange={setSearch}/>
-                <DropDownOptions selected={region} onSelectionChange={setRegion}/>
-            </header>
-            <main className={style.countries}>
-                {
-                    countries
-                        .filter(country => region === "" || region === country.region)
-                        .filter(country => lowerCaseSearch === ""
-                            || country.lowerCaseNameIncludes(lowerCaseSearch)
-                            || country.lowerCaseCapitalsInclude(lowerCaseSearch))
-                        .map(country => <Card key={country.name} country={country}/>)
-                        .slice(0, 8)
-                }
-            </main>
-        </>
+        <MainPage/>
     )
 }
 
---
+++ 15-rest-countries-api-with-color-theme-switcher/rest-countries-api-with-color-theme-switcher-react/src/pages/MainPage.jsx
@@ -0,0 +1,45 @@
+import MenuBar from "../components/MenuBar.jsx";
+import SearchBar from "../components/SearchBar.jsx";
+import DropDownOptions from "../components/DropDownOptions.jsx";
+import Card from "../components/Card.jsx";
+import style from "./MainPage.module.scss";
+import {useEffect, useState} from "react";
+import CountriesApi from "../api/CountriesApi.js";
+
+function MainPage() {
+    const [countries, setCountries] = useState([])
+    const [region, setRegion] = useState("")
+    const [search, setSearch] = useState("")
+    const lowerCaseSearch = search.toLowerCase()
+
+    useEffect(() => {
+        const countriesApi = new CountriesApi(import.meta.env.VITE_COUNTRIES_API)
+        countriesApi
+            .getAll()
+            .then(setCountries)
+    }, []);
+
+    return (
+        <>
+            <MenuBar/>
+            <header className={style.header}>
+                <SearchBar value={search} onValueChange={setSearch}/>
+                <DropDownOptions selected={region} onSelectionChange={setRegion}/>
+            </header>
+            <main className={style.countries}>
+                {
+                    countries
+                        .filter(country => region === "" || region === country.region)
+                        .filter(country => lowerCaseSearch === ""
+                            || country.lowerCaseNameIncludes(lowerCaseSearch)
+                            || country.lowerCaseCapitalsInclude(lowerCaseSearch))
+                        .map(country => <Card key={country.name} country={country}/>)
+                        .slice(0, 8)
+                }
+            </main>
+
+        </>
+    )
+}
+
+export default MainPage
