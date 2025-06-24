24a8315ca1d1843beb6804098cadcb11e7abc3a8
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-25 00:24:35 +0200

    Memory game, fix clock started twice

--- 16-memory-game/memory-game-react/src/hooks/useClock.ts
+++ 16-memory-game/memory-game-react/src/hooks/useClock.ts
@@ -1,26 +1,26 @@
-import {useState} from "react";
+import {useRef} from "react";
 
 /**
  * Custom hook to manage a clock that ticks every second.
  * @param onTick - Callback function to be called every second when the clock ticks.
  */
 function useClock(onTick: () => void) {
-    const [clockId, setClockId] = useState<number | null>(null);
+    let ref = useRef<number>(0);
 
     const start = () => {
-        if (!clockId) {
+        if (!ref.current) {
             console.log("Starting clock")
             const id = setInterval(onTick, 1000)
             console.log("started clock", id)
-            setClockId(id)
+            ref.current = id
         }
     }
 
     const stop = () => {
-        if (clockId) {
-            console.log("Stopping clock", clockId)
-            clearInterval(clockId)
-            setClockId(null)
+        if (ref.current) {
+            console.log("Stopping clock", ref.current)
+            clearInterval(ref.current)
+            ref.current = 0
         }
     }
 
