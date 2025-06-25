fc8b728962cc2e23b8bdca2f7eda6e077d8c18b9
Antoine Belvire <antoine.belvire@opensuse.org>
2025-06-25 19:39:34 +0200

    Memory game, in multiplayer mode, stats should show pairs not moves

--- 16-memory-game/memory-game-react/src/components/MenuStatsMultiplayer.tsx
+++ 16-memory-game/memory-game-react/src/components/MenuStatsMultiplayer.tsx
@@ -1,20 +1,20 @@
 import {StatCount} from "./Stat.tsx";
 import style from "./MenuStatsMultiplayer.module.scss"
 
-function MenuStatsMultiplayer({players}: { players: Array<{ id: number, moves: number }> }) {
+function MenuStatsMultiplayer({players}: { players: Array<{ id: number, pairs: number }> }) {
     return (
         <div className={style.menuMultiplayer}>
             {players.map(player =>
-                <PlayerCard key={`card-${player.id}`} id={player.id} moves={player.moves}/>
+                <PlayerCard key={`card-${player.id}`} id={player.id} pairs={player.pairs}/>
             )}
         </div>
     )
 }
 
-function PlayerCard({id, moves}: { id: number, moves: number }) {
+function PlayerCard({id, pairs}: { id: number, pairs: number }) {
     return (
         <div className={style.playerCard}>
-            <StatCount label={`P${id}`} count={moves} direction="column"/>
+            <StatCount label={`P${id}`} count={pairs} direction="column"/>
         </div>
     )
 }
