c55521f994decb96882db4f71fc80b6bc9adb6ee
Antoine Belvire <antoine.belvire@opensuse.org>
2025-07-01 22:34:26 +0200

    Memory game, menu bar, tablet & desktop

--- 16-memory-game/memory-game-react/src/components/ButtonPrimary.tsx
+++ 16-memory-game/memory-game-react/src/components/ButtonPrimary.tsx
@@ -1,12 +1,23 @@
 import style from "./ButtonPrimary.module.scss"
 import type {ReactNode} from "react";
 
-function ButtonPrimary({type, onClick, children}: {
+function ButtonPrimary({type, onClick, children, big = false}: {
     type?: "button" | "submit" | "reset",
     onClick?: () => void,
-    children: ReactNode
+    children: ReactNode,
+    big?: boolean
 }) {
-    return <button className={style.buttonPrimary} type={type} onClick={onClick}>{children}</button>
+    let styles = style.buttonPrimary
+    if (big) {
+        styles += ` ${style["buttonPrimary--big"]}`
+    }
+    return (
+        <button className={styles}
+                type={type}
+                onClick={onClick}>
+            {children}
+        </button>
+    )
 }
 
 export default ButtonPrimary
