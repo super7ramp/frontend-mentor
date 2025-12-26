3fdda9a023249081c3dd48dcda4d8bd25454985f
Antoine Belvire <antoine.belvire@opensuse.org>
2025-12-26 14:44:44 +0100

    flashcard, refactor

---
+++ 17-flashcard-app/flashcard-uix/src/app/components/card.cljs
@@ -0,0 +1,24 @@
+(ns app.components.card
+  (:require [app.components.progressbar :refer [progress-bar]]
+            [uix.core :as uix :refer [defui $]]))
+
+(defui card-recto [{:keys [question category knownCount set-revealed]}]
+  ($ :button.card {:on-click #(set-revealed true)}
+     ($ :p.card__category.text-preset-6 category)
+     ($ :div.card__body
+        ($ :p.text-preset-1--mobile question)
+        ($ :p.card__heading.text-preset-4--medium "Click to reveal answer"))
+     ($ progress-bar {:known-count knownCount})))
+
+(defui card-verso [{:keys [answer category knownCount set-revealed]}]
+  ($ :button.card.card--verso {:on-click #(set-revealed false)}
+     ($ :p.card__category.text-preset-6 category)
+     ($ :div.card__body
+        ($ :p.card__heading.text-preset-4--medium "Answer:")
+        ($ :p.text-preset-1--mobile answer))
+     ($ progress-bar {:known-count knownCount})))
+
+(defui card [{:keys [revealed] :as card-data}]
+  (if-not revealed
+    ($ card-recto card-data)
+    ($ card-verso card-data)))
