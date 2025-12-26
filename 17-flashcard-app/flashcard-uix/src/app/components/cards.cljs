e2d2fd080a87ea4aadab1f47a2e7c2d0f6b7386a
Antoine Belvire <antoine.belvire@opensuse.org>
2025-12-26 14:10:51 +0100

    flashcard, progressbar

--- 17-flashcard-app/flashcard-uix/src/app/components/cards.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/cards.cljs
@@ -1,6 +1,6 @@
 (ns app.components.cards
-  (:require [uix.core :as uix :refer [defui $ use-effect use-state]]
-            [uix.dom]))
+  (:require [app.components.progressbar :refer [progress-bar]]
+            [uix.core :as uix :refer [defui $ use-effect use-state]]))
 
 (defn- fetch-data []
   (-> (js/fetch "data/data.json")
@@ -9,22 +9,36 @@
       (.then #(:flashcards %))
       (.catch #(js/console.log %))))
 
-(defui card-not-revealed [{:keys [question category set-revealed]}]
+(defui card-recto [{:keys [question category knownCount set-revealed]}]
   ($ :button.card {:on-click #(set-revealed true)}
      ($ :p.card__category.text-preset-6 category)
-     ($ :p.text-preset-1--mobile question)
-     ($ :p.card__heading.text-preset-4--medium "Click to reveal answer")))
+     ($ :div.card__body
+        ($ :p.text-preset-1--mobile question)
+        ($ :p.card__heading.text-preset-4--medium "Click to reveal answer"))
+     ($ progress-bar {:known-count knownCount})))
 
-(defui card-revealed [{:keys [answer category set-revealed]}]
-  ($ :button.card.card--revealed {:on-click #(set-revealed false)}
+(defui card-verso [{:keys [answer category knownCount set-revealed]}]
+  ($ :button.card.card--verso {:on-click #(set-revealed false)}
      ($ :p.card__category.text-preset-6 category)
-     ($ :p.card__heading.text-preset-4--medium "Answer:")
-     ($ :p.text-preset-1--mobile answer)))
+     ($ :div.card__body
+        ($ :p.card__heading.text-preset-4--medium "Answer:")
+        ($ :p.text-preset-1--mobile answer))
+     ($ progress-bar {:known-count knownCount})))
 
 (defui card [{:keys [revealed] :as card-data}]
   (if-not revealed
-    ($ card-not-revealed card-data)
-    ($ card-revealed card-data)))
+    ($ card-recto card-data)
+    ($ card-verso card-data)))
+
+(defui card-interactor [{:keys [card-data set-known-count]}]
+  (let [known-count (:knownCount card-data)
+        inc-known-count #(when (< known-count 5) (set-known-count (inc known-count)))
+        reset-known-count #(set-known-count 0)]
+    ($ :div.card-interactor
+       ($ card card-data)
+       ($ :div.card-buttons
+          ($ :button.card-buttons__i-know-this {:on-click inc-known-count} "I Know This")
+          ($ :button.card-buttons__reset {:on-click reset-known-count} "Reset Progress")))))
 
 (defui card-selector [{:keys [current set-current total]}]
   (let [select-previous #(set-current (mod (dec current) total))
@@ -43,7 +57,8 @@
                        :set-revealed set-current-revealed)
         _ (use-effect #(-> (fetch-data) (.then set-deck)) [])]
     ($ :div.cards
-       ($ card current)
+       ($ card-interactor {:card-data current
+                           :set-known-count #(set-deck (assoc-in deck [current-index :knownCount] %))})
        ($ card-selector {:current current-index
                          :set-current #(do (set-current-index %)
                                            (set-current-revealed false))
