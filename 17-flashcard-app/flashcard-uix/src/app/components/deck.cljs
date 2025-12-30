a267cd72e300f6363e49812341c08333a7f81027
Antoine Belvire <antoine.belvire@opensuse.org>
2025-12-30 19:36:03 +0100

    flashcard app, extract deck transformer

--- 17-flashcard-app/flashcard-uix/src/app/components/deck.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/deck.cljs
@@ -1,56 +1,11 @@
 (ns app.components.deck
   (:require [app.components.card :refer [card]]
-            [app.components.select :refer [select]]
+            [app.components.deck-transformer :refer [deck-transformer]]
             [app.hooks.use-cards :refer [use-cards]]
             [app.hooks.use-filter-list :refer [use-filter-list]]
             [app.hooks.use-selector :refer [use-selector]]
             [app.utils :refer [mastered?]]
             [uix.core :refer [defui $ use-state]]))
-
-(defn- select-categories-button-label [options]
-  (let [option-count (count options)
-        selected-count (count (filter :selected options))]
-    (cond
-      (= selected-count option-count) "All Categories"
-      (= 1 selected-count) "One Category"
-      :else (str selected-count " Categories"))))
-
-(defui select-categories [{:keys [category-frequencies selected-categories set-selected-categories]}]
-  (let [options (->> category-frequencies
-                     (map (fn [[category freq]]
-                            {:value category
-                             :complement (str "(" freq ")")
-                             :selected (or (= selected-categories ::all-categories)
-                                           (contains? selected-categories category))})))]
-    ($ select {:id "select-categories"
-               :label (select-categories-button-label options)
-               :options options
-               :on-change (fn [category selected]
-                            (set-selected-categories
-                             (cond
-                               selected (conj selected-categories category)
-                               (= selected-categories ::all-categories) (disj (set (keys category-frequencies)) category)
-                               (> (count selected-categories) 1) (disj selected-categories category)
-                               :else ::all-categories)))})))
-
-(defui deck-transformer [{:keys [category-frequencies
-                                 selected-categories
-                                 set-selected-categories
-                                 shuffle
-                                 mastered-hidden
-                                 set-mastered-hidden]}]
-  ($ :div.deck-transformer
-     ($ :div.deck-filter
-        ($ select-categories {:category-frequencies category-frequencies
-                              :selected-categories selected-categories
-                              :set-selected-categories set-selected-categories})
-        ($ :div.deck-filter__hide-mastered
-           ($ :input {:type "checkbox"
-                      :id "hide-mastered"
-                      :value mastered-hidden
-                      :on-click #(set-mastered-hidden (not mastered-hidden))})
-           ($ :label {:for "hide-mastered"} "Hide Mastered")))
-     ($ :button.deck-transformer__shuffle {:on-click shuffle} "Shuffle")))
 
 (defui card-interactor [{:keys [card-data set-known-count] :as props}]
   (let [known-count (:knownCount card-data)
@@ -71,12 +26,12 @@
 (defui deck []
   (let [[cards set-cards] (use-cards)
 
-        [selected-categories set-selected-categories] (use-state ::all-categories)
+        [selected-categories set-selected-categories] (use-state :all-categories)
         [mastered-hidden set-mastered-hidden] (use-state false)
         [filtered-cards original-index-of] (use-filter-list
                                             #(and (or (not mastered-hidden)
                                                       (not (mastered? %)))
-                                                  (or (= selected-categories ::all-categories)
+                                                  (or (= selected-categories :all-categories)
                                                       (contains? selected-categories (:category %))))
                                             cards)
 
