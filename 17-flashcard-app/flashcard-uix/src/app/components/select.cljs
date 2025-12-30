586f21c416d5d9349856a3f259249f7cb0d00259
Antoine Belvire <antoine.belvire@opensuse.org>
2025-12-30 15:47:51 +0100

    flashcard app, select cleanup

--- 17-flashcard-app/flashcard-uix/src/app/components/select.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/select.cljs
@@ -1,24 +1,22 @@
 (ns app.components.select
-  (:require [uix.core :refer [$ defui ]]))
+  (:require [uix.core :refer [$ defui]]))
 
 (defui option [{:keys [value complement selected on-change]}]
   ($ :li.select__option
-     ($ :input.text-preset-5 {:type "checkbox"
+     ($ :input {:type "checkbox"
                 :id value
                 :checked selected
                 :on-change #(on-change value (not selected))})
      ($ :label.text-preset-5 {:for value}
         value " " ($ :span.select__option-label-complement complement))))
 
-(defui select-list [{:keys [id options on-change]}]
-  ($ :ul.select__list {:popover "" :id id}
-     (->> options
-          (map (fn [{:keys [value] :as item}]
-                 ($ option (assoc item
-                                  :key value
-                                  :on-change on-change)))))))
-
-(defui select [{:keys [id label] :as props}]
+(defui select
+  [{:keys [id label options on-change]}]
   ($ :div.select
      ($ :button.select__button {:popover-target id} label)
-     ($ select-list props)))
+     ($ :ul.select__list {:popover "" :id id}
+        (->> options
+             (map (fn [{:keys [value] :as item}]
+                    ($ option (assoc item
+                                     :key value
+                                     :on-change on-change))))))))
