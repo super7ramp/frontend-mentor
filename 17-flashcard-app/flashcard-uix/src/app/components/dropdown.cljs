f0b60ec2eb345e965ab669e9b79ae362c4f4c7e6
Antoine Belvire <antoine.belvire@opensuse.org>
2026-01-01 20:04:00 +0100

    flashcard app, fix missing keys in lists

--- 17-flashcard-app/flashcard-uix/src/app/components/dropdown.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/dropdown.cljs
@@ -1,12 +1,17 @@
 (ns app.components.dropdown
   (:require [uix.core :refer [$ defui]]))
 
-(defui dropdown [{:keys [id class-name children]}]
+(defui dropdown-entry [{:keys [key children]}]
+  ($ :li.dropdown__entry {:key key}
+     children))
+
+(defui dropdown
+  "A dropdown. Children should be dropdown entries or at least have a key."
+  [{:keys [id class-name children]}]
   (let [anchor-name (str "--dropdown-anchor-" id)]
     ($ :div {:style {:anchor-name anchor-name}}
        ($ :ul.dropdown {:id id
                         :class-name class-name
                         :popover ""
                         :style {:position-anchor anchor-name}}
-          (map (fn [child] ($ :li.dropdown__entry child))
-               children)))))
+          children))))
