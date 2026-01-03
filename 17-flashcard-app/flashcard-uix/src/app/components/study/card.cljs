0be62f8c49c0c5624918169a9aa34d6fe7829268
Antoine Belvire <antoine.belvire@opensuse.org>
2026-01-03 16:15:28 +0100

    flashcard app, more animations

--- 17-flashcard-app/flashcard-uix/src/app/components/study/card.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/study/card.cljs
@@ -3,26 +3,25 @@
             [app.components.progress-bar :refer [progress-bar]]
             [uix.core :as uix :refer [defui $]]))
 
-(defui card-recto [{:keys [question category knownCount set-revealed]}]
-  ($ :button.card {:on-click #(set-revealed true)}
-     ($ badge category)
-     ($ :div.card__body
-        ($ :p.text-preset-1 question)
-        ($ :p.card__heading.text-preset-4--medium "Click to reveal answer"))
-     ($ progress-bar {:known-count knownCount})))
+(defui card-body-recto [{:keys [question]}]
+  ($ :<>
+     ($ :p.text-preset-1 question)
+     ($ :p.card__secondary-text.text-preset-4--medium "Click to reveal answer")))
 
-(defui card-verso [{:keys [answer category knownCount set-revealed]}]
-  ($ :button.card.card--verso {:on-click #(set-revealed false)}
-     ($ badge category)
-     ($ :div.card__body
-        ($ :p.card__heading.text-preset-4--medium "Answer:")
-        ($ :p.text-preset-2 answer))
-     ($ progress-bar {:known-count knownCount})))
+(defui card-body-verso [{:keys [answer]}]
+  ($ :<>
+     ($ :p.card__secondary-text.text-preset-4--medium "Answer:")
+     ($ :p.text-preset-2 answer)))
 
 (defui card
   "A flashcard ⚡ Two sides: recto contains the question, verso contains the answer."
   [{:keys [card-data revealed set-revealed]}]
-  (let [card-data-and-set-revealed (assoc card-data :set-revealed set-revealed)]
-    (if-not revealed
-      ($ card-recto card-data-and-set-revealed)
-      ($ card-verso card-data-and-set-revealed))))
+  (let [{:keys [category question answer knownCount]} card-data]
+    ($ :button.card {:on-click #(set-revealed (not revealed)) 
+                     :class-name (when revealed "card--verso")}
+       ($ badge category)
+       ($ :div.card__body
+          (if-not revealed
+            ($ card-body-recto {:question question})
+            ($ card-body-verso {:answer answer})))
+       ($ progress-bar {:known-count knownCount}))))
