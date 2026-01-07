ef89f3b66665d48424acba9ec758c03f76583665
Antoine Belvire <antoine.belvire@opensuse.org>
2026-01-07 19:22:50 +0100

    flashcard app, css fixes

--- 17-flashcard-app/flashcard-uix/src/app/components/study/card_selector.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/study/card_selector.cljs
@@ -5,6 +5,6 @@
   ($ :div.card-selector
      ($ :button.card-selector-button.card-selector__previous {:title "Previous" :on-click select-previous}
         ($ :span.card-selector-button__text "Previous"))
-     ($ :p "Card " (inc current) " of " total)
+     ($ :p.card-selector__text "Card " (inc current) " of " total)
      ($ :button.card-selector-button.card-selector__next {:title "Next" :on-click select-next}
         ($ :span.card-selector-button__text "Next"))))
