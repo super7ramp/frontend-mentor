f6b509f0bf424baaaaa84ace856c29d21a912cbc
Antoine Belvire <antoine.belvire@opensuse.org>
2026-01-16 11:22:54 +0100

    flashcard app, memoize context

--- 17-flashcard-app/flashcard-uix/src/app/contexts/cards.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/contexts/cards.cljs
@@ -1,8 +1,9 @@
 (ns app.contexts.cards
   (:require [app.hooks.use-local-storage :refer [use-local-storage]]
             [app.models.card :as model]
+            [app.utils :refer [find-first]]
             [cljs.spec.alpha :as s]
-            [uix.core :refer [$ create-context defui use-callback use-effect use-state]]))
+            [uix.core :refer [$ create-context defui use-callback use-effect use-memo use-state]]))
 
 (defn- fetch-data []
   (-> (js/fetch "data/data.json")
@@ -16,7 +17,7 @@
 (s/def ::stored-cards (s/nilable (s/coll-of ::model/card)))
 
 (defui cards-provider
-  "Provides, via the `*cards*` context, the cards retrieved from local storage or remote API."
+  "Provides, via `*cards-context*`, the cards retrieved from local storage or remote API."
   [{:keys [children]}]
   (let [[cards set-cards] (use-state [])
         [get-stored-cards store-cards] (use-local-storage "cards" ::stored-cards)
@@ -26,6 +27,23 @@
         _ (use-effect #(if-let [stored-cards (get-stored-cards)]
                          (set-cards stored-cards)
                          (-> (fetch-data) (.then set-and-store-cards)))
-                      [get-stored-cards set-and-store-cards])]
-    ($ *cards-context* {:value [cards set-and-store-cards]}
+                      [get-stored-cards set-and-store-cards])
+        context (use-memo (fn []
+                            {:cards cards
+                             
+                             :update-card (fn [{:keys [id] :as updated-card}]
+                                            (when-let [index (find-first #(= id (:id %)) cards)]
+                                              (set-and-store-cards (assoc cards index updated-card))))
+                             
+                             :add-card (fn [new-card]
+                                         (let [new-card-with-id (assoc new-card :id (str (random-uuid)) :knownCount 0)]
+                                           (set-and-store-cards (conj cards new-card-with-id))))
+                             
+                             :delete-card (fn [{:keys [id]}]
+                                            (when-let [index (find-first #(= id (:id %)) cards)] 
+                                              (set-and-store-cards (into (subvec cards 0 index) (subvec cards (inc index))))))
+                             
+                             :shuffle #(set-and-store-cards (shuffle cards))})
+                          [cards set-and-store-cards])] 
+    ($ *cards-context* {:value context}
        children)))
--- 17-flashcard-app/flashcard-uix/src/app/hooks/use_cards.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/hooks/use_cards.cljs
@@ -1,24 +1,8 @@
 (ns app.hooks.use-cards
   (:require [app.contexts.cards :refer [*cards-context*]]
-            [app.utils :refer [find-first]]
             [uix.core :refer [defhook use-context]]))
 
 (defhook use-cards
   "Hook to read and write global cards data."
   []
-  (let [[cards set-cards] (use-context *cards-context*)]
-    {:cards cards
-
-     :update-card (fn [{:keys [id] :as updated-card}]
-                    (when-let [index (find-first #(= id (:id %)) cards)]
-                      (set-cards (assoc cards index updated-card))))
-
-     :add-card (fn [new-card]
-                 (let [new-card-with-id (assoc new-card :id (str (random-uuid)) :knownCount 0)]
-                   (set-cards (conj cards new-card-with-id))))
-
-     :delete-card (fn [{:keys [id]}]
-                    (when-let [index (find-first #(= id (:id %)) cards)]
-                      (set-cards (into (subvec cards 0 index) (subvec cards (inc index))))))
-
-     :shuffle #(set-cards (shuffle cards))}))
+  (use-context *cards-context*))
