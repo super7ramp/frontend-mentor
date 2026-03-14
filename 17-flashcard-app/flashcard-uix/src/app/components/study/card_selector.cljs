24c91edf260991623a290b493a4a75784b061e90
Antoine Belvire <antoine.belvire@opensuse.org>
2026-03-14 23:56:05 +0100

    flashcard app, titles

--- 17-flashcard-app/flashcard-uix/src/app/components/study/card_selector.cljs
+++ 17-flashcard-app/flashcard-uix/src/app/components/study/card_selector.cljs
@@ -3,12 +3,12 @@
 
 (defui card-selector [{:keys [current total select-previous select-next]}]
   ($ :div.card-selector
-     ($ :button.card-selector-button.card-selector__previous {:title "Previous" :on-click select-previous}
-        ($ :span.card-selector-button__text 
+     ($ :button.card-selector-button.card-selector__previous {:title "Go to previous card" :on-click select-previous}
+        ($ :span.card-selector-button__text
            "Previous"))
      ($ :p.card-selector__text {:aria-live "polite"
                                 :aria-atomic true}
         "Card " (inc current) " of " total)
-     ($ :button.card-selector-button.card-selector__next {:title "Next" :on-click select-next}
-        ($ :span.card-selector-button__text 
+     ($ :button.card-selector-button.card-selector__next {:title "Go to next card" :on-click select-next}
+        ($ :span.card-selector-button__text
            "Next"))))
