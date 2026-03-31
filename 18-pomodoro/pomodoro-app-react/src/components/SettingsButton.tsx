c9284b3d94e52fc64f86fad597bf8f93dc902398
Antoine Belvire <antoine.belvire@opensuse.org>
2026-03-31 14:47:02 +0200

    pomodora app, settings button

---
+++ 18-pomodoro/pomodoro-app-react/src/components/SettingsButton.tsx
@@ -0,0 +1,13 @@
+import iconSettingsSvg from '../assets/icons/icon-settings.svg';
+
+import './SettingsButton.scss';
+
+const SettingsButton = (props: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => {
+    return (
+        <button className={`settings-button ${props.className}`} {...props}>
+            <img src={iconSettingsSvg} />
+        </button>
+    );
+}
+
+export default SettingsButton;
