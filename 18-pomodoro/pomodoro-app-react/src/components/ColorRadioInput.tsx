5cf4e70b859e45291b8553fa701eaa29c3cce2e0
Antoine Belvire <antoine.belvire@opensuse.org>
2026-04-01 19:48:06 +0200

    pomodoro app, extract radio inputs

---
+++ 18-pomodoro/pomodoro-app-react/src/components/ColorRadioInput.tsx
@@ -0,0 +1,24 @@
+import './ColorRadioInput.scss';
+
+type ColorRadioInputProps = {
+    /** the identifier of the color */
+    color: string,
+    /** whether the input is checked */
+    checked: boolean,
+    /** callback when input is clicked */
+    onClick: () => void,
+}
+
+const ColorRadioInput = ({color, checked, onClick}: ColorRadioInputProps) => {
+    return (
+        <input
+            aria-label={color}
+            className={`color-radio-input color-radio-input--${color}`}
+            name='color'
+            type='radio'
+            checked={checked}
+            onClick={onClick} />
+    )
+}
+
+export default ColorRadioInput;
--- 18-pomodoro/pomodoro-app-react/src/components/SettingsDialog.tsx
+++ 18-pomodoro/pomodoro-app-react/src/components/SettingsDialog.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import iconCloseSvg from '../assets/icons/icon-close.svg';
 import useSettings from '../hooks/useSettings';
+import ColorRadioInput from './ColorRadioInput';
+import FontRadioInput from './FontRadioInput';
 import NumberInput from './NumberInput';
 
 import './SettingsDialog.scss';
@@ -81,28 +83,22 @@ const SettingsDialog = ({ id }: SettingsDialogProps) => {
                 <fieldset>
                     <h3>Font</h3>
                     <div className='settings-dialog__fonts'>
-                        {currentSettings?.font.fonts.map((font, index) => (
-                            <div className={`settings-dialog__font settings-dialog__font--${font}`} key={font}>
-                                <div aria-hidden={true}>Aa</div>
-                                <input
-                                    aria-label={`Font ${index + 1}`}
-                                    name='font'
-                                    type="radio"
-                                    checked={currentSettings?.font.selected === font}
-                                    onClick={() => selectFont(font)} />
-                            </div>
+                        {currentSettings?.font.fonts.map((font) => (
+                            <FontRadioInput
+                                key={font}
+                                font={font}
+                                checked={currentSettings?.font.selected === font}
+                                onClick={() => selectFont(font)} />
                         ))}
                     </div>
                 </fieldset>
                 <fieldset>
                     <h3>Color</h3>
                     <div className='settings-dialog__colors'>
-                        {currentSettings?.color.colors.map((color, index) => (
-                            <input
-                                aria-label={`Color ${index + 1}`}
-                                className={`settings-dialog__color settings-dialog__color--${color}`}
-                                name='color'
-                                type="radio"
+                        {currentSettings?.color.colors.map((color) => (
+                            <ColorRadioInput
+                                key={color}
+                                color={color}
                                 checked={currentSettings?.color.selected === color}
                                 onClick={() => selectColor(color)} />
                         ))}
