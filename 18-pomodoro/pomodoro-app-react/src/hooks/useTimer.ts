460da1a526ca95489931ae6a12b7a0203820ee39
Antoine Belvire <antoine.belvire@opensuse.org>
2026-04-05 00:04:50 +0200

    pomodoro app, improve useTimer

--- 18-pomodoro/pomodoro-app-react/src/hooks/useTimer.ts
+++ 18-pomodoro/pomodoro-app-react/src/hooks/useTimer.ts
@@ -1,21 +1,16 @@
 import { useEffect, useState } from "react";
 
-const useTimer = (duration: number) => {
-  const [remaining, setRemaining] = useState(duration);
+const useTimer = (durationInSeconds: number) => {
+  const [remaining, setRemaining] = useState(durationInSeconds);
   const [paused, setPaused] = useState(true);
   const finished = remaining <= 0;
-  const reset = () => setRemaining(duration);
+  const reset = () => setRemaining(durationInSeconds);
 
   useEffect(() => {
-    if (finished) {
-      return;
+    if (!finished && !paused) {
+      const timerId = setInterval(() => setRemaining((n) => n - 1), 1000);
+      return () => clearInterval(timerId);
     }
-    const timerId = setInterval(() => {
-      if (!paused) {
-        setRemaining((n) => n - 1);
-      }
-    }, 1000);
-    return () => clearInterval(timerId);
   }, [finished, paused]);
 
   return { remaining, paused, setPaused, finished, reset };
